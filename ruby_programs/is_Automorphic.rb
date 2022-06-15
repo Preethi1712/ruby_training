f47e9c5407ae72392191867d82876b3754dfa217
Preethi1712 <preethi@drylogics.com>
2022-06-15 15:54:00 +0530

    review changes

--- ruby_programs/is_Automorphic.rb
+++
@@ -1,6 +0,0 @@
-def is_Automorphic?(number) 
-  s = number.to_s
-  square = number * number
-  last = square % (10 ** s.length)
-  return last == number
-end
--- ruby_programs/is_prime.rb
+++ ruby_programs/is_prime.rb
@@ -1,13 +1,9 @@
 def is_prime?(number)
   prime = true
-  if number == 0 || number == 1
-    return false
+  return false if [0, 1].include?(number)
+
+  for n in 2..number - 1
+    prime = false if number % n == 0
   end
-  n = 2
-  for n in 2..number-1
-  if number % n == 0
-  prime = false
-  end
-end
   prime
 end
